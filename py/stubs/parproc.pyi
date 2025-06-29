113b844a64ebf8320eba616d408032341de245dc
Øivind Loe <oivind.loe@gmail.com>
2025-06-29 10:34:15 -0500

    stubs for parproc

---
+++ py/stubs/parproc.pyi
@@ -0,0 +1,65 @@
+from typing import Any, Callable, TypeVar, Union, overload
+
+F = TypeVar('F', bound=Callable[..., Any])
+
+class Proc:
+    """Decorator for processes"""
+
+    @overload
+    def __init__(
+        self,
+        name: str | None = None,
+        *,
+        deps: list[str] | None = None,
+        locks: list[str] | None = None,
+        now: bool = False,
+        args: dict[str, Any] | None = None,
+        proto: Any | None = None,
+        timeout: float | None = None,
+    ) -> None: ...
+
+    @overload
+    def __init__(
+        self,
+        name: str | None = None,
+        f: F | None = None,
+        *,
+        deps: list[str] | None = None,
+        locks: list[str] | None = None,
+        now: bool = False,
+        args: dict[str, Any] | None = None,
+        proto: Any | None = None,
+        timeout: float | None = None,
+    ) -> None: ...
+
+    def __call__(self, f: F) -> F: ...
+
+class Proto:
+    """Decorator for process prototypes"""
+
+    @overload
+    def __init__(
+        self,
+        name: str | None = None,
+        *,
+        deps: list[str] | None = None,
+        locks: list[str] | None = None,
+        now: bool = False,
+        args: dict[str, Any] | None = None,
+        timeout: float | None = None,
+    ) -> None: ...
+
+    @overload
+    def __init__(
+        self,
+        name: str | None = None,
+        f: F | None = None,
+        *,
+        deps: list[str] | None = None,
+        locks: list[str] | None = None,
+        now: bool = False,
+        args: dict[str, Any] | None = None,
+        timeout: float | None = None,
+    ) -> None: ...
+
+    def __call__(self, f: F) -> F: ...
