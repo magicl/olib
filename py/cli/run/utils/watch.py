d0fed467c33d12bfbd12ff85ddf8e1f5ccdcd32b
Øivind Loe <oivind.loe@gmail.com>
2025-07-19 20:09:03 -0500

    file watch util

---
+++ py/cli/run/utils/watch.py
@@ -0,0 +1,25 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# Copyright 2024 Øivind Loe
+# See LICENSE file or http://www.apache.org/licenses/LICENSE-2.0 for details.
+# ~
+from collections.abc import Callable
+
+import sh
+
+
+def watch_files(path: str, cwd: str, func: Callable[[], None]) -> None:
+    """
+    Watch a path for changes and run a function when it changes
+    """
+
+    try:
+        for line in sh.inotifywait('-m', '-r', '-e', 'close_write', path, _cwd=cwd, _iter=True):
+            print('Updated: ', line)
+            try:
+                func()
+            except sh.ErrorReturnCode:
+                # Ignore build errors. Keep listening
+                continue
+
+    except KeyboardInterrupt:
+        print('Stopping watch')
--- scripts/init-rust.sh
+++ scripts/init-rust.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Licensed under the Apache License, Version 2.0 (the "License");
+# Copyright 2024 Øivind Loe
+# See LICENSE file or http://www.apache.org/licenses/LICENSE-2.0 for details.
+# ~
 
 # Install Rust
 curl https://sh.rustup.rs -sSf | sh
